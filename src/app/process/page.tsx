917c34d3abb2d43a4a7feba6e45fef09ad60e139
Jhonatas Anicezio <jhonatasanic@hotmail.com>
2023-06-06 15:28:25 -0300

    feat: create prototype for process

---
+++ src/app/process/page.tsx
@@ -0,0 +1,79 @@
+export default function Process() {
+  return (
+    <main className="flex flex-col text-brown-color items-center justify-center">
+      <div className="flex flex-col gap-5 py-20 items-center text-center w-5/6 ">
+        <h1 className="text-6xl font-light">Real estate, simplified</h1>
+        <h2 className="text-xl">UNDERSTAND THE BUYING & SELLING PROCESS</h2>
+        <p className="text-sm w-3/4">Buying or selling your home is a big undertaking - partnering with a Realtor® means you are working with an expert to guide you through the process and set you up for success.</p>
+        <div className="flex gap-10">
+          <button className="px-10 rounded-full border border-brown-color">
+            BUYING A HOME
+          </button>
+          <button className="px-10 rounded-full border border-brown-color">
+            SELLING A HOME
+          </button>
+        </div>
+      </div>
+      <div className="flex py-10 w-11/12">
+        <div className="flex flex-col p-6 gap-3">
+          <h2 className="text-5xl">The Home Buying Process</h2>
+          <h3 className="text-lg">THE ESSENTIAL STEPS TO BUY A HOME IN MEDICINE HAT</h3>
+          <p className="text-sm">Looking to purchase a home? Look no further. We've laid out the 4 essential steps to buying a home below, to help break down the process. As your real estate expert in Medicine Hat, we guide you through this process and make sure you are satisfied every step of the way.</p>
+        </div>
+        <img className="w-1/2 h-96 p-6" src="/kends-patisserie.webp" alt="Descrição da imagem" />
+      </div>
+      <div className="flex text-black text-3xl pt-4 pb-16 w-5/6">
+        <div className="flex flex-col gap-4 items-center text-center border-r py-10 w-1/4 px-5 border-black">
+          <h2 className="flex items-center justify-center text-3xl rounded-full h-20 w-20 border border-brown-color">1</h2>
+          <h3>Decide to buy a new home</h3>
+          <p className="text-base">The first step to buying a new home is the decision to start looking. Once you've made that decision, it is important to be pre-approved for a mortgage so you know your budget, and consider how much you can put towards a down payment.</p>
+        </div>
+        <div className="flex flex-col gap-4 items-center text-center border-r py-10 w-1/4 px-5 border-black">
+          <h2 className="flex items-center justify-center text-3xl rounded-full h-20 w-20 border border-brown-color">2</h2>
+          <h3>Decide to buy a new home</h3>
+          <p className="text-base">The first step to buying a new home is the decision to start looking. Once you've made that decision, it is important to be pre-approved for a mortgage so you know your budget, and consider how much you can put towards a down payment.</p>
+        </div>
+        <div className="flex flex-col gap-4 items-center text-center border-r py-10 w-1/4 px-5 border-black">
+          <h2 className="flex items-center justify-center text-3xl rounded-full h-20 w-20 border border-brown-color">3</h2>
+          <h3>Decide to buy a new home</h3>
+          <p className="text-base">The first step to buying a new home is the decision to start looking. Once you've made that decision, it is important to be pre-approved for a mortgage so you know your budget, and consider how much you can put towards a down payment.</p>
+        </div>
+        <div className="flex flex-col gap-4 items-center text-center py-10 w-1/4 px-5">
+          <h2 className="flex items-center justify-center text-3xl rounded-full h-20 w-20 border border-brown-color">4</h2>
+          <h3>Decide to buy a new home</h3>
+          <p className="text-base">The first step to buying a new home is the decision to start looking. Once you've made that decision, it is important to be pre-approved for a mortgage so you know your budget, and consider how much you can put towards a down payment.</p>
+        </div>
+      </div>
+      <div className="flex justify-center py-10 gap-5 w-11/12">
+        <div className="w-2/5 h-80 bg-cover" style={{ backgroundImage: "url('/bolo-cubo-magico.webp')" }}/>
+        <div className="flex flex-col w-1/2 p-5 gap-3">
+          <h2 className="text-5xl">The Home Buying Process</h2>
+          <h3 className="text-lg">THE ESSENTIAL STEPS TO BUY A HOME IN MEDICINE HAT</h3>
+          <p className="text-sm">Looking to purchase a home? Look no further. We've laid out the 4 essential steps to buying a home below, to help break down the process. As your real estate expert in Medicine Hat, we guide you through this process and make sure you are satisfied every step of the way.</p>
+        </div>
+      </div>
+      <div className="flex text-black text-3xl pt-4 pb-28 w-5/6">
+        <div className="flex flex-col gap-4 items-center text-center border-r py-10 w-1/4 px-5 border-black">
+          <h2 className="flex items-center justify-center text-3xl rounded-full h-20 w-20 border border-brown-color">1</h2>
+          <h3>Decide to buy a new home</h3>
+          <p className="text-base">The first step to buying a new home is the decision to start looking. Once you've made that decision, it is important to be pre-approved for a mortgage so you know your budget, and consider how much you can put towards a down payment.</p>
+        </div>
+        <div className="flex flex-col gap-4 items-center text-center border-r py-10 w-1/4 px-5 border-black">
+          <h2 className="flex items-center justify-center text-3xl rounded-full h-20 w-20 border border-brown-color">2</h2>
+          <h3>Decide to buy a new home</h3>
+          <p className="text-base">The first step to buying a new home is the decision to start looking. Once you've made that decision, it is important to be pre-approved for a mortgage so you know your budget, and consider how much you can put towards a down payment.</p>
+        </div>
+        <div className="flex flex-col gap-4 items-center text-center border-r py-10 w-1/4 px-5 border-black">
+          <h2 className="flex items-center justify-center text-3xl rounded-full h-20 w-20 border border-brown-color">3</h2>
+          <h3>Decide to buy a new home</h3>
+          <p className="text-base">The first step to buying a new home is the decision to start looking. Once you've made that decision, it is important to be pre-approved for a mortgage so you know your budget, and consider how much you can put towards a down payment.</p>
+        </div>
+        <div className="flex flex-col gap-4 items-center text-center py-10 w-1/4 px-5">
+          <h2 className="flex items-center justify-center text-3xl rounded-full h-20 w-20 border border-brown-color">4</h2>
+          <h3>Decide to buy a new home</h3>
+          <p className="text-base">The first step to buying a new home is the decision to start looking. Once you've made that decision, it is important to be pre-approved for a mortgage so you know your budget, and consider how much you can put towards a down payment.</p>
+        </div>
+      </div>
+    </main>
+  );
+}
