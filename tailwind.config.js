1c9c4a2d414f44b74488df0dee11fc2aaa0f395e
Jhonatas Anicezio <jhonatasanic@hotmail.com>
2023-06-15 19:02:18 -0300

    feat: add new color

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,11 +8,24 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        'brown-color': '#564B4B',
+        brown: {
+          '50': '#f7f3ec',
+          '100': '#e2d3be',
+          '200': '#c2ab8f',
+          '300': '#a28360',
+          '350': '#564B4B',
+          '400': '#825a31',
+          '500': '#633203',
+          '600': '#512702',
+          '700': '#3f1e02',
+          '800': '#2e1501',
+          '900': '#2C2424',
+        },
       },
       spacing: {
         'banner': '37rem',
-        'banner-regular': '35rem'
+        'banner-regular': '35rem',
+        'space-header': '8.5rem'
       },
     },
   },
