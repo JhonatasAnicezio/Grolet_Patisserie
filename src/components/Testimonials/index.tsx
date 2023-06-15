28f04545a9b280a753de7a6699b8d50fe0df5742
Jhonatas Anicezio <jhonatasanic@hotmail.com>
2023-06-15 19:03:44 -0300

    feat: create component Testimonials

---
+++ src/components/Testimonials/index.tsx
@@ -0,0 +1,9 @@
+export function Testimonials() {
+  return (
+    <div className="flex flex-col px-5 py-10 gap-10 rounded-3xl items-center text-center w-5/6 text-brown-350 bg-white">
+      <h2 className="text-2xl pb-5 w-7/12 border-b border-black">Clientes</h2>
+      <p className="text-3xl px-10">Os doces dessa patisserie são simplesmente divinos! A variedade de opções é impressionante e a qualidade dos ingredientes é evidente em cada mordida. Mal posso esperar para experimentar mais sabores!</p>
+      <h3 className="text-xl">Amanda</h3>
+    </div>
+  )
+}
