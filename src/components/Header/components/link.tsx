84c463539e58f4a997528eff99fef109770856be
Jhonatas Anicezio <jhonatasanic@hotmail.com>
2023-06-16 22:54:58 -0300

    feat: create component link for Header

---
+++ src/components/Header/components/link.tsx
@@ -0,0 +1,24 @@
+'use client'
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+
+interface propsLinkHeader {
+  name: string,
+  route: string,
+}
+
+export function LinkHeader({ route, name }: propsLinkHeader) {
+  const pathname = usePathname();
+
+  return (
+    <Link
+      className={
+        `transition duration-500 hover:text-brown-350 hover:border-b-2 hover:border-brown-350
+        ${pathname === route && `text-brown-350 border-b-2 border-brown-350`}`
+      }
+      href={route}
+    >
+      {name}
+    </Link>
+  )
+}
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import { AiOutlinePhone } from "react-icons/ai";
 import { FaInstagram, FaFacebookSquare, FaEnvelope } from "react-icons/fa";
+import { LinkHeader } from "./components/link";
 
 export function Header() {
   return (
@@ -12,7 +13,7 @@ export function Header() {
         </div>
         <nav className="flex w-28 text-xl justify-between">
           <Link target="blank" href='https://www.instagram.com/cedricgroletopera/'>
-            <FaInstagram className="transition duration-500 marker:hover:text-pink-500" />
+            <FaInstagram className="transition duration-500 hover:text-pink-500" />
           </Link>
           <Link target="blank" href='https://www.facebook.com/cedricgrolet'>
             <FaFacebookSquare className="transition duration-500 hover:text-blue-400" />
@@ -28,10 +29,10 @@ export function Header() {
           <h2 className="font-medium">SABORES QUE ENCANTAM, MOMENTOS QUE FICAM</h2>
         </Link>
         <nav className="flex w-2/5 justify-between items-center font-semibold text-sm text-gray-500">
-          <Link className="transition duration-500 hover:text-brown-350 hover:border-b-2 hover:border-brown-350" href='/about'>SOBRE</Link>
-          <Link className="transition duration-500 hover:text-brown-350 hover:border-b-2 hover:border-brown-350" href='/recipes'>RECEITAS</Link>
-          <Link className="transition duration-500 hover:text-brown-350 hover:border-b-2 hover:border-brown-350" href='/process'>PROCESSO</Link>
-          <Link className="transition duration-500 hover:text-brown-350 hover:border-b-2 hover:border-brown-350" href='/contact'>CONTATO</Link>
+          <LinkHeader name="SOBRE" route="/about" />
+          <LinkHeader name="RECEITAS" route="/recipes" />
+          <LinkHeader name="PROCESSO" route="/process" />
+          <LinkHeader name="CONTATO" route="/contact" />
           <button className="transition duration-500 rounded-full py-1 px-5 border border-gray-500 hover:bg-brown-350 hover:text-white hover:border-brown-350">
             ENTRAR / CADASTRO
           </button>
