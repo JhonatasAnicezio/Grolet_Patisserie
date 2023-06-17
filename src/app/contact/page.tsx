02e2aed7f38af7f86f0aad7ba352211d9c54c641
Jhonatas Anicezio <jhonatasanic@hotmail.com>
2023-06-16 22:57:07 -0300

    feat: add component Message in contact

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,10 +1,11 @@
 import Link from "next/link";
+import Message from '../../components/Form/Message';
 import { BsFacebook, BsFillEnvelopeFill, BsFillTelephoneFill, BsInstagram } from "react-icons/bs";
 
 export default function Contact() {
   return (
     <main className="flex flex-col items-center  pt-space-header">
-      <div className="flex flex-col gap-3 py-16 items-center text-center text-brown-color w-4/5">
+      <div className="flex flex-col gap-3 py-16 items-center text-center text-brown-350 w-4/5">
         <h1 className="text-6xl">Entrar em contato</h1>
         <h2>CONTATO COM O NOSSO TIME DE ATENDIMENTO</h2>
         <p className="text-sm px-20">Está pronto para encomendar algo especial e exclusivo? Queremos ouvir suas ideias e transformá-las em realidade! Entre em contato conosco hoje mesmo e vamos trabalhar juntos para criar um pedido personalizado que atenda perfeitamente às suas expectativas.</p>
@@ -47,12 +48,13 @@ export default function Contact() {
           </div>
         </div>
       </div>
+      <Message />
       <div className="flex flex-col items-center justify-center bg-cover h-banner w-full" style={{ backgroundImage: "url('/frutas.webp')" }}>
-        <div className="flex flex-col gap-5 p-11 items-center text-center text-brown-color rounded-3xl w-11/12 bg-white">
+        <div className="flex flex-col gap-5 p-11 items-center text-center text-brown-350 rounded-3xl w-11/12 bg-white">
           <h2 className="text-6xl font-light">Marque uma consulta gratuita</h2>
           <h3>NÃO SABE POR ONDE COMEÇAR? VAMOS CONVERSAR!</h3>
           <p className="px-7">Agende uma consulta através do link abaixo para uma conversa de 15 minutos, onde discutiremos suas preferências, expectativas e como podemos criar delícias personalizadas para tornar seus momentos especiais ainda mais memoráveis.</p>
-          <button className="transition duration-500 px-10 py-0.5 rounded-full hover:bg-brown-900 bg-brown-color text-white">AGENDE UMA CHAMADA AGORA</button>
+          <button className="transition duration-500 px-10 py-0.5 rounded-full hover:bg-brown-900 bg-brown-350 text-white">AGENDE UMA CHAMADA AGORA</button>
         </div>
       </div>
     </main>
