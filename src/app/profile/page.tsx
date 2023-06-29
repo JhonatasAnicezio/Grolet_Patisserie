b0c8307666ca1ec6454340f480a646f10240795d
Jhonatas Anicezio <jhonatasanic@hotmail.com>
2023-06-29 11:01:33 -0300

    feat: create page profile

---
+++ src/app/profile/page.tsx
@@ -0,0 +1,9 @@
+import { CardProfile } from "@/components/CardProfile";
+
+export default function Profile() {
+  return (
+    <main className="flex pt-space-header justify-center items-center">
+      <CardProfile />
+    </main>
+  );
+}
