0e0c50d17f3e606d4557239930dec23bbbe30b01
Jhonatas Anicezio <jhonatasanic@hotmail.com>
2023-07-03 15:36:40 -0300

    feat: implements recipes

--- src/app/recipes/page.tsx
+++ src/app/recipes/page.tsx
@@ -1,7 +1,19 @@
-export default function Recipes() {
+import { getRecipes } from "@/services/recipes";
+
+export default async function Recipes() {
+  const { meals } = await getRecipes();
+
   return (
-    <main className="pt-space-header">
-      Recipes
+    <main className="flex flex-col items-center pt-space-header">
+      <h1 className="text-4xl text-brown-350 py-7">Receitas</h1>
+      <div className="flex flex-wrap justify-between w-3/4">
+        {meals?.map((e, index) => (
+          <div key={index} className="flex flex-col text-brown-350 pb-8 justify-center w-[30%]">
+            <img src={e.strMealThumb} className="w-full h-44" />
+            <h2>{e.strMeal}</h2>
+          </div>
+        ) )}
+      </div>
     </main>
   )
 ;}
