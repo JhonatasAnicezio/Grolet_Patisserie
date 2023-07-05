47f96c82ff79fb5c30abd8624782dd671f0131b0
Jhonatas Anicezio <jhonatasanic@hotmail.com>
2023-07-05 10:54:55 -0300

    feat: add link for components

--- src/app/recipes/page.tsx
+++ src/app/recipes/page.tsx
@@ -1,4 +1,5 @@
 import { getRecipes } from "@/services/recipes";
+import Link from "next/link";
 
 export default async function Recipes() {
   const { meals } = await getRecipes();
@@ -8,12 +9,14 @@ export default async function Recipes() {
       <h1 className="text-4xl text-brown-350 py-7">Receitas</h1>
       <div className="flex flex-wrap justify-between w-3/4">
         {meals?.map((e, index) => (
-          <div key={index} className="flex flex-col text-brown-350 pb-8 justify-center w-[30%]">
-            <img src={e.strMealThumb} className="w-full h-44" />
-            <h2>{e.strMeal}</h2>
+          <div id={e.idMeal} key={index} className="flex flex-col text-brown-350 pb-8 justify-center w-[30%]">
+            <Link href={`/recipes/${e.idMeal}`}>
+              <img src={e.strMealThumb} className="w-full h-44" />
+              <h2>{e.strMeal}</h2>
+            </Link>
           </div>
         ) )}
       </div>
     </main>
   )
-;}
+}
