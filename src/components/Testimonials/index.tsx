fffddd9f744e9e5a36f2d7ccd9574f672eec1f9f
Jhonatas Anicezio <jhonatasanic@hotmail.com>
2023-07-05 10:52:18 -0300

    refact: alter css

--- src/components/Testimonials/index.tsx
+++ src/components/Testimonials/index.tsx
@@ -2,7 +2,7 @@ export function Testimonials() {
   return (
     <div className="flex flex-col px-5 py-10 gap-5 rounded-3xl items-center text-center w-5/6 text-brown-350 bg-white">
       <h2 className="text-xl pb-2 w-7/12 border-b border-black">Clientes</h2>
-      <p className="text-2xl px-10">Os doces dessa patisserie são simplesmente divinos! A variedade de opções é impressionante e a qualidade dos ingredientes é evidente em cada mordida. Mal posso esperar para experimentar mais sabores!</p>
+      <p className="text-2xl w-11/12 px-10">Os doces dessa patisserie são simplesmente divinos! A variedade de opções é impressionante e a qualidade dos ingredientes é evidente em cada mordida. Mal posso esperar para experimentar mais sabores!</p>
       <h3 className="text-base">Amanda</h3>
     </div>
   )
