c2f323172e3d861c87343689b4570c2d38fa0e7c
Jhonatas Anicezio <jhonatasanic@hotmail.com>
2023-07-05 15:36:52 -0300

    refact: alter css

--- src/app/recipes/page.tsx
+++ src/app/recipes/page.tsx
@@ -5,14 +5,15 @@ export default async function Recipes() {
   const { meals } = await getRecipes();
 
   return (
-    <main className="flex flex-col items-center pt-space-header">
-      <h1 className="text-4xl text-brown-350 py-7">Receitas</h1>
-      <div className="flex flex-wrap justify-between w-3/4">
+    <main className="flex flex-col text-brown-350 gap-3 items-center pt-space-header">
+      <h1 className="text-4xl pt-7">Receitas</h1>
+      <p className="pb-7">As melhores receitas você só encontra aqui.</p>
+      <div className="flex flex-wrap gap-10 justify-between w-3/4">
         {meals?.map((e, index) => (
-          <div id={e.idMeal} key={index} className="flex flex-col text-brown-350 pb-8 justify-center w-[30%]">
+          <div id={e.idMeal} key={index} className="flex flex-col justify-center w-[28%]">
             <Link href={`/recipes/${e.idMeal}`}>
               <img src={e.strMealThumb} className="w-full h-44" />
-              <h2>{e.strMeal}</h2>
+              <h2 className="py-2">{e.strMeal}</h2>
             </Link>
           </div>
         ) )}
