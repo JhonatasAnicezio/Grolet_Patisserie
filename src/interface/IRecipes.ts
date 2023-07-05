73be39c70a4c9e52bf6097a6a63b68a8850d059b
Jhonatas Anicezio <jhonatasanic@hotmail.com>
2023-07-05 10:51:27 -0300

    feat: add type for details

--- src/interface/IRecipes.ts
+++ src/interface/IRecipes.ts
@@ -7,3 +7,15 @@ export type Recipe = {
   strMeal: string,
   strMealThumb: string,
 }
+
+export type ResponseDetails = {
+  meals: [RecipeDetails],
+}
+
+export type RecipeDetails = {
+  idMeal: string,
+  strMeal: string,
+  strMealThumb: string,
+  strInstructions: string,
+  strYoutube: string,
+}
