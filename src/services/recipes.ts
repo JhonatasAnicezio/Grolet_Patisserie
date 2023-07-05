c1be489b022dd16d8b99670f8d11b12ca7ddbbbc
Jhonatas Anicezio <jhonatasanic@hotmail.com>
2023-07-05 10:50:54 -0300

    feat: create api for recipes

--- src/services/recipes.ts
+++ src/services/recipes.ts
@@ -1,11 +1,21 @@
-import { Recipes } from '@/interface/IRecipes';
+import { RecipeDetails, Recipes, ResponseDetails } from '@/interface/IRecipes';
 import { apiRecipes } from './api';
 
 export async function getRecipes(): Promise<Recipes> {
   try {
-    const { data } = await apiRecipes.get<Recipes>('/filter.php?c=Dessert',);
+    const { data } = await apiRecipes.get<Recipes>('/filter.php?c=Dessert');
 
     return data;
+  } catch (error) {
+    throw new Error('Erro ao obter usuário');
+  }
+}
+
+export async function getRecipeDetails(id: string): Promise<RecipeDetails> {
+  try {
+    const { data: { meals } } = await apiRecipes.get<ResponseDetails>(`/lookup.php?i=${id}`);
+
+    return meals[0];
   } catch (error) {
     throw new Error('Erro ao obter usuário');
   }
