fe38d014f76954f882508f4ca6929b08b168d467
Jhonatas Anicezio <jhonatasanic@hotmail.com>
2023-07-06 18:50:50 -0300

    feat: alter css for components

--- src/components/Header/components/button.tsx
+++ src/components/Header/components/button.tsx
@@ -8,7 +8,7 @@ import { UserIcon } from "./userIcon";
 export function Button() {
   const [isOpen, setIsOpen] = useState(false)
 
-  const { token, isFetching } = useContext(AuthContext);
+  const { token } = useContext(AuthContext);
 
   const pathname = usePathname();
 
@@ -20,7 +20,6 @@ export function Button() {
           onClick={() => setIsOpen(!isOpen)}
           className={`
             transition duration-500 rounded-full py-1 px-3 border border-gray-500 hover:bg-brown-350 hover:text-white hover:border-brown-350
-            ${isFetching && 'bg-gray-500 text-gray-500 duration-0'}
             ${pathname === '/register' && 'bg-brown-350 text-white border-brown-350'}
           `}
         >
