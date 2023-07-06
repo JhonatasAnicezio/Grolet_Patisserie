e2b4a04f21e0537ce1e0c503ab5ea9c3e5eb0627
Jhonatas Anicezio <jhonatasanic@hotmail.com>
2023-07-05 21:13:25 -0300

    feat: implements ReciéDetails

--- src/interface/IRecipes.ts
+++ src/interface/IRecipes.ts
@@ -13,9 +13,15 @@ export type ResponseDetails = {
 }
 
 export type RecipeDetails = {
-  idMeal: string,
-  strMeal: string,
-  strMealThumb: string,
-  strInstructions: string,
-  strYoutube: string,
-}
+  idMeal: string;
+  strMeal: string;
+  strMealThumb: string;
+  strInstructions: string;
+  strYoutube: string;
+  ingredients: string[],
+  measures: string[],
+} & {
+  [key in `strIngredient${1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12 | 13 | 14 | 15 | 16 | 17 | 18 | 19 | 20}`]: string;
+} & {
+  [key in `strMeasure${1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12 | 13 | 14 | 15 | 16 | 17 | 18 | 19 | 20}`]: string;
+};
