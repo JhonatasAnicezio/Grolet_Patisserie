6af6563c354b18921ec497b8fecdfbf5eafdcd52
Jhonatas Anicezio <jhonatasanic@hotmail.com>
2023-07-05 21:14:23 -0300

    feat: create api for RecipeDetails

--- src/services/recipes.ts
+++ src/services/recipes.ts
@@ -13,9 +13,30 @@ export async function getRecipes(): Promise<Recipes> {
 
 export async function getRecipeDetails(id: string): Promise<RecipeDetails> {
   try {
-    const { data: { meals } } = await apiRecipes.get<ResponseDetails>(`/lookup.php?i=${id}`);
+    const data = await apiRecipes.get<ResponseDetails>(`/lookup.php?i=${id}`)
+      .then(({ data: { meals } }) => {        
+          const ingredients: string[] = [];
+          const measures: string[] = [];
 
-    return meals[0];
+          const meal = meals[0];
+
+          for (let i = 1; i <= 20; i++) {
+            const ingredientKey = `strIngredient${i}`;
+            const measureKey = `strMeasure${i}`;
+
+            if (meal[ingredientKey as keyof RecipeDetails]) {
+              const ingredient = meal[ingredientKey as keyof RecipeDetails];
+              const measure = meal[measureKey as keyof RecipeDetails] || ""; // Trate o valor da medida se for nulo
+
+              ingredients.push(ingredient as string);
+              measures.push(measure as string);
+            }
+          }
+
+          return { ...meal, ingredients, measures }
+      });
+
+    return data;
   } catch (error) {
     throw new Error('Erro ao obter usuário');
   }
