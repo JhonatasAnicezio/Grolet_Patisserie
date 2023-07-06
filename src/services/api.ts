0dda91897ca04783a3d5bdd92cd745d27c23c979
Jhonatas Anicezio <jhonatasanic@hotmail.com>
2023-07-06 13:42:30 -0300

    feat: add api in deploy

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const apiUser = axios.create({
-  baseURL: 'http://localhost:3001/user',
+  baseURL: 'https://api-patisserie-users.up.railway.app/user',
 });
 
 export const apiRecipes = axios.create({
